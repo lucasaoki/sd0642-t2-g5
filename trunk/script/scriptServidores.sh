d94f21acdd9b9450b592b62b6aac5a061d5c943a
gilberto.vollpe <gilberto.vollpe@25a34ed2-0c75-7367-eb75-aa3ccd8c5af0>
2012-10-11 11:28:30 +0000

    Atualizacao Makefile e scripts

--- trunk/script/scriptServidores.sh
+++ trunk/script/scriptServidores.sh
@@ -9,13 +9,13 @@ pathHosts=${basepath}hosts/
 echo É necessário estar logado no servidor X-men para execucao
 echo do teste.
 
-hostsClients=("0" "ciclope" "wolwerine" "magneto" "hulk" "colossus" "tempestade" "demolidor" "venon" "vampira" "gambit" "elektra" "anjo")
+hostsClients=("0" "ciclope" "wolwerine" "magneto" "hulk" "colossus" "tempestade" "demolidor" "noturno" "vampira" "gambit" "elektra" "anjo")
 
 echo Servidor RPC
-ssh  x-men ./pw2_server >> ${pathServer}serverXMENout.txt &
+ssh -f x-men "./pw2_server >> ${pathServer}serverXMENout.txt " 
 
 for	i in `seq 1 12`; do
 	echo Host $i 
-	ssh ${hostsClients[$i]} ${binfolder}pw2_client x-men >> ${pathHosts}host${i}out.txt 
+	ssh -f ${hostsClients[$i]} "${binfolder}pw2_client x-men >> ${pathHosts}host${i}out.txt" 
 done
 
--- trunk/script/scriptTime.sh
+++ trunk/script/scriptTime.sh
@@ -3,10 +3,15 @@
 base=../
 basepath=${base}doc/out/
 pathHosts=${basepath}hosts/
-pathTime=${basepath}times/
+pathTime=${basepath}'times'/
 
-commandoCut=cut -d ' ' -f 10
+grepVariable=("0" "INSERT" "SELECT" "DELETE");
+
+commandoCut="cut -d ' ' -f 10"
 
 for i in `seq 1 12`; do
+	for j in `seq 1 3`; do
+		grep ${grepVariable${i}} | ${commandoCut} >> ${pathTime}${grepVariable}Hosts${i}.txt	
+	done	
 	${commandoCut} ${pathHosts}host${i}out.txt >> ${pathTime}timeHosts${i}.txt
 done
